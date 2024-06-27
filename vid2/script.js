5ea4b61c7c17f7bae5158761cb16a20db486877d
Wahdaapf <wahdaadella.ba@gmail.com>
2024-06-27 20:53:27 +0700

    handle trigger

--- vid2/script.js
+++ vid2/script.js
@@ -1,7 +1,61 @@
-gsap.from("#page1 .box", {
-    scale: 0,
-    delay: 1,
-    duration: 2,
-    rotate: 360,
+// gsap.from("#page1 .box", {
+//     scale: 0,
+//     delay: 1,
+//     duration: 2,
+//     rotate: 360,
     
+// })
+
+// gsap.from("#page2 .box", {
+//     scale: 0,
+//     delay: 2,
+//     duration: 2,
+//     rotate: 360,
+//     scrollTrigger: {
+//         trigger: "#page2 .box",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 60%"
+//     }
+// })
+
+//************ */
+// gsap.from("#page2 h1", {
+//     opacity: 0,
+//     duration: 2,
+//     x: 500,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroll: "body", 
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+// gsap.from("#page2 h2", {
+//     opacity: 0,
+//     duration: 2,
+//     x: -500,
+//     scrollTrigger: {
+//         trigger: "#page2 h2",
+//         scroll: "body", 
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+gsap.from("#page2 .box", {
+    scale: 0,
+    opacity: 0, 
+    duration: 1,
+    rotate: 720,
+    scrollTrigger: {
+        trigger: "#page2 .box",
+        scroller: "body",
+        markers: true,
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 2, //boolean, number,
+        pin: true
+    }
 })
